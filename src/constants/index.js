15d3e1e5bded7091b2d9b983db2fde511ec8e5b3
fadega <fadega.araya@gmail.com>
2023-07-19 02:29:03 -0600

    Work contact, and feedback sections added

--- src/constants/index.js
+++ src/constants/index.js
@@ -172,27 +172,27 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
+        "Working with them has been a pleasure. They consistently deliver high-quality work that addresses our product needs, and their dedication to the team and the product is truly appreciated.",
+      name: "Derek Van Neuren",
+      designation: "Product Manager",
+      company: "RM Plc",
+      image: "https://randomuser.me/api/portraits/women/44.jpg",
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
+        "Throughout our collaboration, Fadega has consistently shown a high level of technical competence and a strong commitment to our team's objectives. His problem-solving skills have been a great asset in our projects.",
+      name: "David Wyn ",
+      designation: "Lead Developer",
+      company: "RM Plc",
+      image: "https://randomuser.me/api/portraits/men/55.jpg",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
+        "They have a good knack for coding and are always ready to take on challenges. Their positive attitude and willingness to learn make them a great asset to our team. We're lucky to have them",
+      name: "Felica Gu",
+      designation: "Senior Analyst Programmer",
+      company: "RM Plc",
+      image: "https://randomuser.me/api/portraits/women/66.jpg",
     },
   ];
   
