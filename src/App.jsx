a16ca37dfa3ea274040640a9d5ea1dae4f74b6fe
fadega <fadega.araya@gmail.com>
2023-09-04 01:57:39 -0600

    AM-1001

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary bg-[#0A192f]">
+      <div className="relative z-0 bg-[#0A192f]">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
